cf4220f4605fd5a14130c4e3966b693906ad1fe5
Rama Krishna <siddarth21@hotmail.com>
2016-10-18 16:43:18 +0800

    added user model and bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :password,
+  length: { in: 8..72 },
+  on: :create
+
+  has_secure_password
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+
+end
